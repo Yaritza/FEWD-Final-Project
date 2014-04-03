a1d0204054d4c24569009306c7cd3da9bddf19e8
Yaritza <saturn540@gmail.com>
2014-04-03 16:31:27 -0400

    fixed directory.  Slidder is up, but needs styling

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
-<script language="JavaScript1.1">
-var photos=new Array()
-var which=0
+$(document).ready(function(){
+    // Set the interval to be 5 seconds
+    var t = setInterval(function(){
+        $(".photos ul").animate({marginLeft:-480},1000,function(){
+            $(this).find("li:last").after($(this).find("li:first"));
+            $(this).css({marginLeft:0});
+        })
+    },5000);
+});
 
-/*Change the below variables to reference your own images. You may have as many images in the slider as you wish*/
-photos[0]="/img/largeCrowdpic.jpg" alt="Image of me deejaying on stage before a large crowd for the opening of the JukeBox the Ghost Concert"
-photos[1]="../img/weddingPic1.jpg" alt="Picture of happy wedding guests dancing with a bride and groom at an indoor reception in New Jersey"
-photos[2]="../img/poolParty.jpg" alt="Image of a party crowd around a large pool as seen from my DJ booth set up on the 2nd story balcony - Atlantic City New Jersey"
-photos[3]="../img/highlineFundraiser.jpg" alt="Gathering of people at the HighLine Park Fundraiser in New York City"
-photos[4]="../img/schoolDance.jpg"
 
 
-function backward(){
-if (which>0){
-window.status=''
-which--
-document.images.photoslider.src=photos[which]
-}
-}
+//  Code source: http://www.my-html-codes.com/easy-jquery-carousel/
 
-function forward(){
-if (which<photos.length-1){
-which++
-document.images.photoslider.src=photos[which]
-}
-else window.status='End of gallery'
-}
-</script>
+
+
+
+
+
