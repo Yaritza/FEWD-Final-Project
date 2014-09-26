6a56da7ac965856b52d5c8166f72d49e39fe4e37
Yaritza <saturn540@gmail.com>
2014-09-26 18:57:01 -0400

    added more background images, fixed link from logo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     // Set the interval to be 5 seconds
-    var t = setInterval(function(){
-        $(".photos ul").animate({marginLeft:-480},1000,function(){
-            $(this).find("li:last").after($(this).find("li:first"));
-            $(this).css({marginLeft:0});
-        })
-    },5000);
+    // var t = setInterval(function(){
+    //     $(".photos ul").animate({marginLeft:-480},1000,function(){
+    //         $(this).find("li:last").after($(this).find("li:first"));
+    //         $(this).css({marginLeft:0});
+    //     })
+    // },5000);
 });
 
 
